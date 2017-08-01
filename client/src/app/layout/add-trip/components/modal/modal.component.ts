0fcf2e594dfec732d9d4d61564907dd673c4fb72
umberto <umbertocicero@gmail.com>
2017-08-01 15:34:28 +0200

    modal

--- client/src/app/layout/add-trip/components/modal/modal.component.ts
+++ client/src/app/layout/add-trip/components/modal/modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, Output, EventEmitter, ElementRef} from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, ElementRef } from '@angular/core';
 import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 import { Stop } from './../../../../shared/dto';
 
@@ -7,17 +7,13 @@ import { Stop } from './../../../../shared/dto';
     selector: 'app-modal-add-stop',
     templateUrl: './modal.component.html',
     styleUrls: ['./modal.component.scss'],
-    
-    
+
+
 })
 export class ModalComponent implements OnInit {
 
+    ngOnInit() {
 
-
-
-
-  ngOnInit() {
-     
     }
 
 
@@ -53,31 +49,40 @@ export class ModalComponent implements OnInit {
     //     var files = event.srcElement.files;
     //     console.log(files);
     // }
+    fileChanged(e: Event) {
+        var target: HTMLInputElement = e.target as HTMLInputElement;
+        for (var i = 0; i < target.files.length; i++) {
+            this.upload2(target.files[i]);
+        }
+    }
 
- upload() {
-    //locate the file element meant for the file upload.
+    upload2(img: File) {
+        console.log(img)
+    }
+    upload() {
+        //locate the file element meant for the file upload.
         let inputEl: HTMLInputElement = this.el.nativeElement.querySelector('#photo');
-    //get the total amount of files attached to the file input.
+        //get the total amount of files attached to the file input.
         let fileCount: number = inputEl.files.length;
-    //create a new fromdata instance
+        //create a new fromdata instance
         let formData = new FormData();
-    //check if the filecount is greater than zero, to be sure a file was selected.
+        //check if the filecount is greater than zero, to be sure a file was selected.
         if (fileCount > 0) { // a file was selected
             //append the key name 'photo' with the first file in the element
-                formData.append('photo', inputEl.files.item(0));
-                /*
-            //call the angular http method
-            this.http
-        //post the form data to the url defined above and map the response. Then subscribe //to initiate the post. if you don't subscribe, angular wont post.
-                .post(URL, formData).map((res:Response) => res.json()).subscribe(
-                //map the success function and alert the response
-                 (success) => {
-                         alert(success._body);
-                },
-                (error) => alert(error))
-                */
-          }
-       }
+            formData.append('photo', inputEl.files.item(0));
+            /*
+        //call the angular http method
+        this.http
+    //post the form data to the url defined above and map the response. Then subscribe //to initiate the post. if you don't subscribe, angular wont post.
+            .post(URL, formData).map((res:Response) => res.json()).subscribe(
+            //map the success function and alert the response
+             (success) => {
+                     alert(success._body);
+            },
+            (error) => alert(error))
+            */
+        }
+    }
 
 
 
