b1a19ad2920970b81d3d65b31e625ceb038cfb04
umberto <umbertocicero@gmail.com>
2017-09-01 10:42:43 +0200

    settings

--- server/settings.js
+++ server/settings.js
@@ -7,7 +7,7 @@ module.exports = {
     // host: 'mongodb',
      host: '127.0.0.1',
     port: 27017,
-    name: 'justjsblogdemo'
+    name: 'hawk'
   },
   // Port for the webserver to listen on
   http: {
