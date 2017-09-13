ffaa4497f8b40d12fa40e34f10f7b4c6a2dea4c0
umberto <umbertocicero@gmail.com>
2017-09-13 15:09:11 +0200

    commit

--- client/src/app/layout/home/home.component.ts
+++ client/src/app/layout/home/home.component.ts
@@ -15,8 +15,34 @@ export class HomeComponent implements OnInit {
     ) { }
 
     trips = new Array();
+    private base64Images = [[]];
+    private maxImgNum = 3;
 
     ngOnInit() {
-        this.services.getTrip(null).subscribe(data => { this.trips = data });
+        this.services.getTrip(null).subscribe(data => {
+            var trips = this.trips = data
+            var self = this;
+
+            trips.forEach(trip => {
+
+                if (trip && trip.stops != null && trip.stops.length > 0) {
+                    trip.stops.forEach(stop => {
+                        if (stop.images != null && stop.images.length > 0) {
+                            stop.images.forEach(image => {
+                                this.services.getImage(image, 'small').subscribe(data => {
+                                    var tmp = self.base64Images[trip.id];
+                                    if(!tmp) tmp = self.base64Images[trip.id] = [];
+                                    if (tmp.length < self.maxImgNum) {
+                                        self.base64Images[trip.id].push(data);
+                                    }
+                                });
+                            });
+                        }
+                    });
+                }
+            });
+        });
+
+
     }
 }
